106aa9be6a6cd81fe0bf0ce288535146dae18bde
Guobo <gbjie@thoughtworks.com>
2015-05-15 16:05:27 +0800

    impl 1st req, add length.value logic in the equals

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -3,13 +3,28 @@
  * Created by twer on 5/15/15.
  */
 public class Length {
+    public Length(int n) {
+        _value = n;
+    }
+
+    public Length() {
+    }
+
     @Override
     public boolean equals(Object obj) {
-        return true;
+        if(this == obj)
+            return true;
+
+        if (obj instanceof Length)
+            return _value == ((Length) obj)._value;
+
+        return  false;
     }
 
     @Override
     public int hashCode() {
         return super.hashCode();
     }
+
+    private int _value;
 }
--- src/test/java/LengthTest.java
+++ src/test/java/LengthTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Created by twer on 5/15/15.
@@ -9,5 +10,15 @@ public class LengthTest {
     @Test
     public void should_be_equal_when_compare_two_lengths(){
         assertEquals(new Length(), new Length());
+    }
+
+    @Test
+    public void should_be_equal_when_compare_two_lenghts_with_same_value(){
+        assertEquals(new Length(3), new Length(3));
+    }
+
+    @Test
+    public void should_not_be_equal_when_compare_two_lengths_with_different_values(){
+        assertNotEquals(new Length(2), new Length(1));
     }
 }
