9069c3d80ff46eae694c2c8679f42b89479b3127
Guobo <gbjie@thoughtworks.com>
2015-05-15 15:47:10 +0800

    impl 1st req

---
+++ src/main/java/Length.java
@@ -0,0 +1,15 @@
+
+/**
+ * Created by twer on 5/15/15.
+ */
+public class Length {
+    @Override
+    public boolean equals(Object obj) {
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
---
+++ src/test/java/LengthTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by twer on 5/15/15.
+ */
+public class LengthTest {
+    @Test
+    public void should_be_equal_when_compare_two_lengths(){
+        assertEquals(new Length(), new Length());
+    }
+}
